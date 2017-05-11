bbbb1f6324379994c3ff2a963375193ed844befe
José Pedro Andrés García <macklus@debianitas.net>
2017-05-11 09:34:29 +0200

    .

--- controllers/PaypalController.php
+++ controllers/PaypalController.php
@@ -18,7 +18,7 @@ class PaypalController extends Controller {
     }
 
     public function beforeAction($action) {
-        Yii::$app->request->enableCsrfValidation = false;
+        $this->enableCsrfValidation = false;
         return parent::beforeAction($action);
     }
 
@@ -30,27 +30,28 @@ class PaypalController extends Controller {
 
         file_put_contents($this->_log, "===================\n" . Yii::t('payments', 'New request') . "\n", FILE_APPEND);
 
-        //if (Yii::$app->request->isPost) {
-        $paypal = $this->_module->getMod('paypal');
-        $ipn = new PayPal_Ipn($paypal['live'], $paypal['debug']);
-        if ($ipn->init()) {
-            // Log
-            file_put_contents($this->_log, print_R($ipn, true), FILE_APPEND);
+        if (Yii::$app->request->isPost) {
+            $paypal = $this->_module->getMod('paypal');
+            //$ipn = new PayPal_Ipn($paypal['live'], $paypal['debug']);
+            $ipn = new PayPal_Ipn(false, true);
+            if ($ipn->init()) {
+                // Log
+                file_put_contents($this->_log, print_R($ipn, true), FILE_APPEND);
 
-            //payment_status":"Completed
-            $status = $ipn->getKeyValue('payment_status');
-            if ($status == 'Completed') {
-                file_put_contents($this->_log, 'PAGO COMPLETADO', FILE_APPEND);
+                //payment_status":"Completed
+                $status = $ipn->getKeyValue('payment_status');
+                if ($status == 'Completed') {
+                    file_put_contents($this->_log, 'PAGO COMPLETADO', FILE_APPEND);
+                } else {
+                    file_put_contents($this->_log, 'PAGO INCOMPLETO', FILE_APPEND);
+                }
             } else {
-                file_put_contents($this->_log, 'PAGO INCOMPLETO', FILE_APPEND);
+                file_put_contents($this->_log, "ERROR: ipn->init()", FILE_APPEND);
             }
-        } else {
-            file_put_contents($this->_log, "ERROR: ipn->init()", FILE_APPEND);
         }
-//        }
 
         /* Enable again if you use it */
-        Yii::$app->request->enableCsrfValidation = true;
+        //Yii::$app->request->enableCsrfValidation = true;
         file_put_contents($this->_log, "===================\n", FILE_APPEND);
     }
 
